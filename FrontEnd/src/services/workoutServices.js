bdc7a397f50530222a5ec7d14f88e91315d04309
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-10-30 17:25:50 -0300

    Delete env variable

--- FrontEnd/src/services/workoutServices.js
+++ FrontEnd/src/services/workoutServices.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = `${process.env.REACT_APP_BACKEND_URL}`;
+const baseUrl = "https://fitnessapp-nh5b.onrender.com";
+
 const getAllWorkouts = () => {
   return axios
     .get(`${baseUrl}/api/workouts`)
